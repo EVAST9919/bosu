a5b9d5cc7ea90ee0b5fde56c63aaad0136926d5e
Andrei Zavatski <megaman9919@gmail.com>
2022-03-30 00:11:38 +0300

    Simplify CherryPiece

--- osu.Game.Rulesets.Bosu/Objects/Drawables/Pieces/CherryPiece.cs
+++ osu.Game.Rulesets.Bosu/Objects/Drawables/Pieces/CherryPiece.cs
@@ -1,5 +1,9 @@
-﻿using osu.Framework.Audio.Track;
+﻿using osu.Framework.Allocation;
+using osu.Framework.Audio.Track;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.Textures;
 using osu.Game.Beatmaps.ControlPoints;
 using osu.Game.Graphics.Containers;
 using osu.Game.Rulesets.Bosu.Extensions;
@@ -10,51 +14,76 @@ namespace osu.Game.Rulesets.Bosu.Objects.Drawables.Pieces
 {
     public class CherryPiece : BeatSyncedContainer
     {
-        private Color4 colour;
-
         public new Color4 Colour
         {
-            get => colour;
-            set
-            {
-                colour = value;
-
-                main.Colour = value;
-                additional.Colour = value;
-            }
+            get => cherryBase.Colour;
+            set => cherryBase.Colour = value;
         }
 
-        private readonly CherrySubPiece main;
-        private readonly CherrySubPiece additional;
+        private readonly Sprite cherryBase;
+        private readonly Sprite overlay;
+        private readonly Sprite flash;
+
+        private Texture base1;
+        private Texture base2;
+        private Texture overlay1;
+        private Texture overlay2;
+        private Texture flash1;
+        private Texture flash2;
 
         public CherryPiece()
         {
             Size = new Vector2(IWannaExtensions.CHERRY_DIAMETER);
-            AddRange(new Drawable[]
+            Add(new Container
             {
-                main = new CherrySubPiece(1),
-                additional = new CherrySubPiece(2)
+                Anchor = Anchor.BottomCentre,
+                Origin = Anchor.BottomCentre,
+                Size = IWannaExtensions.CHERRY_FULL_SIZE,
+                Children = new Drawable[]
+                {
+                    cherryBase = new Sprite
+                    {
+                        RelativeSizeAxes = Axes.Both
+                    },
+                    overlay = new Sprite
+                    {
+                        RelativeSizeAxes = Axes.Both
+                    },
+                    flash = new Sprite
+                    {
+                        RelativeSizeAxes = Axes.Both,
+                        Alpha = 0
+                    }
+                }
             });
-
-            applyClockState(true);
         }
 
-        public void Flash(double duration)
+        [BackgroundDependencyLoader]
+        private void load(TextureStore textures)
         {
-            main.Flash(duration);
-            additional.Flash(duration);
+            base1 = textures.Get($"Cherry/cherry-base-1");
+            base2 = textures.Get($"Cherry/cherry-base-2");
+            overlay1 = textures.Get($"Cherry/cherry-overlay-1");
+            overlay2 = textures.Get($"Cherry/cherry-overlay-2");
+            flash1 = textures.Get($"Cherry/cherry-flash-1");
+            flash2 = textures.Get($"Cherry/cherry-flash-2");
+
+            updateTextures(true);
         }
+
+        public void Flash(double duration) => flash.FadeInFromZero(10, Easing.OutQuint).Then().FadeOut(duration);
 
         protected override void OnNewBeat(int beatIndex, TimingControlPoint timingPoint, EffectControlPoint effectPoint, ChannelAmplitudes amplitudes)
         {
             base.OnNewBeat(beatIndex, timingPoint, effectPoint, amplitudes);
-            applyClockState(beatIndex % 2 == 0);
+            updateTextures(beatIndex % 2 == 0);
         }
 
-        private void applyClockState(bool state)
+        private void updateTextures(bool state)
         {
-            main.Alpha = state ? 1 : 0;
-            additional.Alpha = state ? 0 : 1;
+            cherryBase.Texture = state ? base1 : base2;
+            overlay.Texture = state ? overlay1 : overlay2;
+            flash.Texture = state ? flash1 : flash2;
         }
     }
 }
